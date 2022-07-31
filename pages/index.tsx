cff083880cc5c1318429ec3100c9f21e29a13f6b
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-31 03:48:42 +0300

    Importing various components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import type { NextPage } from "next";
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import { Movie } from "../types";
+import requests from "../utils/requests";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -23,15 +25,14 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
       <Head>
         <title>Home - Netflix</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      {/* Header */}
       <main>
-        {/* Banner */}
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* Row */}
           {/* Row */}
