7564f1b3110e1da1dd7cbca92151d4d7306a2a6b
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-31 16:12:57 +0300

    Implement the banner image gradient functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { listenerCount } from "process";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Row from "../components/Row";
@@ -28,7 +27,7 @@ const Home = ({
   horrorMovies,
 }: Props) => {
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
       <Head>
         <title>Home - Netflix</title>
         <link rel='icon' href='/favicon.ico' />
@@ -36,7 +35,7 @@ const Home = ({
       <Header />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className='md:space-y-24'>
           <Row title='Trending Now' movies={trendingNow} />
           <Row title='Top Rated' movies={topRated} />
           <Row title='Action Thrillers' movies={actionMovies} />
